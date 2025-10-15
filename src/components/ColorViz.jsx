bcc01d96a63011567668c6f793da8a6bc0fe6eae
wwatanabe331 <wwatanabe331@gmail.com>
2025-10-15 13:26:23 +0900

    画面修正

---
+++ src/components/ColorViz.jsx
@@ -0,0 +1,10 @@
+import "../styles/colorViz.css";
+function ColorViz() {
+  return (
+    <section className="section">
+      <div>orz</div>
+    </section>
+  );
+}
+
+export default ColorViz;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "../styles/Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1>はなゼミ</h1>
+      <h1 className="site-title">花束作成支援サイト</h1>
     </header>
   );
 }
