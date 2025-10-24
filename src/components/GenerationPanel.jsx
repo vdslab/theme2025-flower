1f3b3b20d23e321e2777b2f7933894432123e689
wwatanabe331 <wwatanabe331@gmail.com>
2025-10-24 15:36:38 +0900

    画像生成の枠を修正

--- src/components/GenerationPanel.jsx
+++ src/components/GenerationPanel.jsx
@@ -1,6 +1,6 @@
-// import "../styles/generationPanel.css";
+import "../styles/generationPanel.css";
 function GenerationPanel() {
-  return <div className="generation">orz</div>;
+  return <div className="generation">画像生成場所</div>;
 }
 
 export default GenerationPanel;
