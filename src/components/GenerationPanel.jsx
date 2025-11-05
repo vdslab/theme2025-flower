a64c27800b7385222a4bccb58ee9a8351e73565b
TomonoHaru <chhr22031@g.nihon-u.ac.jp>
2025-11-05 13:43:13 +0900

    とりあえず直した

--- src/components/GenerationPanel.jsx
+++ src/components/GenerationPanel.jsx
@@ -1,7 +1,11 @@
 import "../styles/generationPanel.css";
 import GeminiApi from "./GeminiAPI";
 const GenerationPanel = ({ flowerList }) => {
-  return <GeminiApi flowerList={flowerList} />;
+  return (
+    <div className="generation">
+      <GeminiApi flowerList={flowerList} />
+    </div>
+  );
 };
 
 export default GenerationPanel;
